9fcb764c779ae6539dbeccefaaf24bcc8f3d7b94
Peter Stangolis <pstangolis@gmail.com>
2023-02-13 12:00:16 -0500

    subtitle formatting

--- 1_🚦_Red_Light_Camera_Data.py
+++ 1_🚦_Red_Light_Camera_Data.py
@@ -212,6 +212,8 @@ with tab3:
     st.markdown("<br>", unsafe_allow_html=True)
     st.markdown(f"<p style = 'font-size:35px;color:#f2f2f2;border-left: 3px solid {light_colors[0]};text-align:center;margin-left:5px;margin-right:5px '>TOP TICKET PRODUCING INTERSECTION</p>", unsafe_allow_html=True)
     st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<p style='font-size:14px;color:#f2f2f2;margin-left:60px;text-align:center;'>TOTAL CHARGES FOR THE YEAR </p>", unsafe_allow_html=True)
+
     st.markdown(f"<h5 style='border-radius:5px; margin-left:100px; margin-right:100px; padding:10px; text-align:center; color:#f2f2f2; font-size:31px;border-style:solid;"
                 f"background-image: linear-gradient(to right, {city_colors[1]}cc, {city_colors[1]}cc,{city_colors[0]}cc);'>  {overall_intersection} </h5>", unsafe_allow_html=True)
     st.markdown("<br>", unsafe_allow_html=True)
@@ -288,7 +290,7 @@ with tab3:
         f"<p style = 'font-size:35px;color:#f2f2f2;border-left: 3px solid {light_colors[2]};text-align:center;"
         f"margin-left:5px;margin-right:5px;padding-left:50px; '>LOCATIONS WITH THE HIGHEST NUMBERS OF TICKETS</p>",
         unsafe_allow_html=True)
-    st.markdown("<p style='font-size:14px;color:#f2f2f2;margin-left:60px;'>TOTAL CHARGES AT EACH LOCATION SINCE THE PROGRAM BEGAN </p>", unsafe_allow_html=True)
+    st.markdown("<p style='font-size:14px;color:#f2f2f2;margin-left:60px;text-align:center;'>TOTAL CHARGES AT EACH LOCATION SINCE THE PROGRAM BEGAN </p>", unsafe_allow_html=True)
 
     st.markdown("<br>", unsafe_allow_html=True)
     col18, col19, col20 = st.columns((2, 0.5, 0.5))
