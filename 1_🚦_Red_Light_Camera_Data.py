4fec47f2047fec0079ebcab14b4e61ca940dd391
Peter Stangolis <pstangolis@gmail.com>
2023-02-13 12:08:00 -0500

    subtitle formatting

--- 1_🚦_Red_Light_Camera_Data.py
+++ 1_🚦_Red_Light_Camera_Data.py
@@ -289,7 +289,7 @@ with tab3:
         f"<p style = 'font-size:35px;color:#f2f2f2;border-left: 3px solid {light_colors[2]};text-align:center;"
         f"margin-left:5px;margin-right:5px;padding-left:50px; '>HIGHEST NUMBERS OF TICKETS</p>",
         unsafe_allow_html=True)
-    st.markdown("<p style='font-size:22px;color:#f2f2f2;margin-left:60px;text-align:center;'>TOTAL CHARGES AT EACH LOCATION SINCE THE PROGRAM BEGAN </p>", unsafe_allow_html=True)
+    st.markdown("<p style='font-size:22px;color:#f2f2f2;margin-left:40px;text-align:center;'>TOTAL CHARGES AT EACH LOCATION SINCE THE PROGRAM BEGAN </p>", unsafe_allow_html=True)
 
     st.markdown("<br>", unsafe_allow_html=True)
     col18, col19, col20 = st.columns((2, 0.5, 0.5))
